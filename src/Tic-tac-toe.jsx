e160ea8d69f7037b0d21f2ae135a154bb4a7e66b
Snehankit Somkuwar <snehankitsomkuwar@Snehankits-MacBook-Air.local>
2025-12-13 22:08:10 +0530

    modified tic-tac-toe.jsx and .css code

--- src/Tic-tac-toe.jsx
+++ src/Tic-tac-toe.jsx
@@ -1,24 +1,43 @@
+import { useState } from 'react';
 import '../css/Tic-tac-toe.css'
 
+function Square({value, onSquareClick}){
+
+    return(
+        <button className='square' onClick={onSquareClick} >
+            {value}
+        </button>
+    );
+}
+
 export default function Board(){
+
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(){
+        const nextSquares = squares.slice();
+        nextSquares[0] = "x";
+        setSquares(nextSquares);
+    }
+
     return(
         <>
-        <div className='square-row'>
-        <button className='square'>1</button>
-        <button className='square'>2</button>
-        <button className='square'>3</button>
+        <div className='board-row'>
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
         </div>
 
-        <div className='square-row'>
-        <button className='square'>4</button>
-        <button className='square'>5</button>
-        <button className='square'>6</button>
+        <div className='board-row'>
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
         </div>
 
-        <div className='square-row'>
-        <button className='square'>7</button>
-        <button className='square'>8</button>
-        <button className='square'>9</button>
+        <div className='board-row'>
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
         </div>
         </>
         
