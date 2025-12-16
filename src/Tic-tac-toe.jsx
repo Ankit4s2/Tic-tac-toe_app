163c869e4c0c6bedd3dc1f4a47a231ea99ff735b
Snehankit Somkuwar <snehankitsomkuwar@Snehankits-MacBook-Air.local>
2025-12-16 17:47:31 +0530

    winner declaration code is done

--- src/Tic-tac-toe.jsx
+++ src/Tic-tac-toe.jsx
@@ -11,16 +11,20 @@ function Square({value, onSquareClick}){
 }
 
 export default function Board(){
-
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i){
+
+        if(squares[i] || calculateWinner(squares)){
+            return;
+        }
+
         if(squares[i]){
             return;
         }
-        const nextSquares = squares.slice();
 
+        const nextSquares = squares.slice();
         if(xIsNext){
             nextSquares[i] = "X";
         }else{
@@ -31,31 +35,19 @@ export default function Board(){
         setXIsNext(!xIsNext);
     }
 
-    function calculateWinner(squares){
-        const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,6,8],
-            [0,4,8],
-            [2,4,6]
-        ];
-        
-        let i=0;
-        while(i<lines.length()){
-            const [a,b,c] = lines[i];
-            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-                return squares[a];
-            } 
-            i++;  
-        }
-        return null;
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner){
+        status = "Winner: " + winner;
+    }else{
+        status = "Next player: " + (xIsNext ? "X" : "O"); 
     }
 
     return(
         <>
+
+        <div className='status' >{status}</div>
+
         <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -77,3 +69,24 @@ export default function Board(){
         
     );
 }
+
+function calculateWinner(squares){
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,6,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+
+        for(let i =0; i<lines.length; i++){
+            const [a,b,c] = lines[i];
+            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a];
+            }   
+        }
+        return null;
+    }
