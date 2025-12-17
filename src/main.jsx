d0f6cd43a9cae61ec0a01d6bc9d516a288ed6cab
Snehankit Somkuwar <somkuwarsnehankit633@gmail.com>
2025-12-17 21:56:11 +0530

    exported a function Game by default having Board as a child component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import MyApp from './App'
-import Board from './Tic-tac-toe.jsx'
+import Game from './Tic-tac-toe.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Board />
+    <Game />
   </StrictMode>,
 )
