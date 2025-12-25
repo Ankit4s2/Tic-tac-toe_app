08d4ec8b23586bf261ed6c5cd7e9db6bc0cc016d
Snehankit Somkuwar <somkuwarsnehankit633@gmail.com>
2025-12-25 17:05:36 +0530

    feat: complete initial tic-tac-toe game (logic still fuzzy)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import MyApp from './App'
 import Game from './Tic-tac-toe.jsx'
 
 createRoot(document.getElementById('root')).render(
